cf3197be95118fc9801afd8ae444e98f058bcdac
Kondratev Alexander <a.kondratev@bi.zone.ru>
2025-10-18 13:14:21 +0300

    add debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
+d() {
+    [ "$LOG_LEVEL" = "DEBUG" ] && echo "[DEBUG] $1"
+}
+d "debug enabled"
 XRAY_CONF_PATH="/usr/local/etc/xray/config.json"
 echo "Starting setup container please wait"
 sleep 1
 
+d "getting SERVER_IP_ADDRESS from ${SERVER_ADDRESS}"
 SERVER_IP_ADDRESS=$(ping -c 1 $SERVER_ADDRESS | awk -F'[()]' '{print $2}')
+d "got SERVER_IP_ADDRESS: ${SERVER_IP_ADDRESS}"
+d "getting NET_IFACE"
 NET_IFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -vE 'lo|tun' | head -n1 | cut -d'@' -f1)
+d "got SERVER_IP_ADDRESS: ${NET_IFACE}"
 
+d "ip tuntap del mode tun dev tun0 || true"
 ip tuntap del mode tun dev tun0 || true
+d "ip tuntap add mode tun dev tun0"
 ip tuntap add mode tun dev tun0
+d "ip addr add 172.31.200.10/30 dev tun0"
 ip addr add 172.31.200.10/30 dev tun0
+d "ip link set dev tun0 up"
 ip link set dev tun0 up
+d "ip route del default via 172.18.20.5 || true"
 ip route del default via 172.18.20.5 || true
+d "ip route add default via 172.31.200.10"
 ip route add default via 172.31.200.10
+d "ip route add "${SERVER_IP_ADDRESS}/32" via 172.18.20.5"
 ip route add "${SERVER_IP_ADDRESS}/32" via 172.18.20.5
 
 echo "nameserver 172.18.20.5" > /etc/resolv.conf
